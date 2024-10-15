1b89c6e0cef5e5be331e2ac26f40423c19708aec
Jaime Quiñones <jaime.quinones.t@gmail.com>
2024-10-14 22:01:44 -0300

    Ia portal done

---
+++ src/components/IAPicker.tsx
@@ -0,0 +1,43 @@
+import CustomButton from './customButton';
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handlerSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyle="text-xs"
+            handleClick={() => false }
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handlerSubmit('logo')}
+              customStyle="text-xs"
+            />
+
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handlerSubmit('full')}
+              customStyle="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default AIPicker
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import CustomButton from './customButton';
+import ColorPicker from './ColorPicker';
+import IAPicker from './IAPicker';
+import FilePicker from './FilePicker';
+import Tab from './Tab';
+
+export { CustomButton, ColorPicker, IAPicker, FilePicker, Tab }
